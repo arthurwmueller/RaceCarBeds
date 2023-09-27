d3ac4bf29d7fb18caa35667d424eed839c2fbab1
unknown <awm21@pitt.edu>
2023-09-26 23:43:12 -0400

    header

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import './header.css'
+
+function Header(){
+    return(
+        <div>
+            <h1>RACECARBEDS.BIZ</h1>
+            <h6>Our inventory is going fast!!!</h6>
+        </div>
+    )
+}
+export default Header
